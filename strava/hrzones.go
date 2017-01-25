d29329b9cd3ca76eb02a8cebef17b0a79f467504
gokhang <Gokhan.Gorali@telenity.com>
2017-01-25 22:57:30 +0300

    New hrzone client

--- strava/hrzones.go
+++ strava/hrzones.go
@@ -5,94 +5,68 @@ import (
    "encoding/json"
    "time"
    "io/ioutil"
-   "os" 
+   "net/http"
+   "os"
 )
 
-type Activity struct {
-   Time            []int 
-   Distance        []int 
-   Altitude        []int
-   Heartrate       []int
-   Cadence         []int
-   Watts           []int
-   Watts_calc      []int
-   Temp            []int
-   Grade_adjusted_distance []int
-   Grade_smooth    []int
-   Moving          []bool
-   Velocity_smooth []int
+type DistributionBucket struct {
+  Max int
+  Min int
+  Time int
+  Percent float64
+  Tag string
+  Label_short string
+  Label_long string
 }
 
-func Bar (n int) string {
-   barChar := "|" 
+type HeartRateZones struct {
+   Score int
+   Distribution_buckets []DistributionBucket
+   Type string
+   Resource_state int
+   Sensor_based bool
+   Points int
+   Custom_zones bool
+   Calculating bool
+}
+
+func Bar (n float64) string {
+   barChar := "|"
    result := ""
    for i := 0; i < int(n/2); i++ {
       result+=barChar
-   }     
+   }
    return result
-} 
-
-func ZonePercentage (zoneTime int, activity Activity) int {
-   return int(float32(zoneTime)/float32(len(activity.Time))*100)
 }
 
 func main() {
-   if len(os.Args[1:]) != 1 {
-      fmt.Println("Usage: hrzones.go <activity json file>")
-      os.Exit(1)
-   }
 
-   jsonFile,_ := ioutil.ReadFile(os.Args[1]);
-   activity := Activity{}
-   json.Unmarshal([]byte(jsonFile), &activity)
-   fmt.Println("HR sample count:", len(activity.Heartrate))
-   
-   z2 := 109
-   z3 := 145
-   z4 := 162
-   z5 := 181 
+  if len(os.Args[1:]) != 1 {
+     fmt.Println("Usage: hrzones2.go <activity id>")
+     os.Exit(1)
+  }
+  client := &http.Client{}
+  req, _ := http.NewRequest("GET", "https://www.strava.com/activities/"+os.Args[1]+"/heartrate_zones", nil)
+  req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0")
+  req.Header.Del("Accept-Encoding")
+  req.Header.Set("Accept", "*/*")
+  res, _ := client.Do(req)
+  defer res.Body.Close()
+  body, _ := ioutil.ReadAll(res.Body)
+  fmt.Println(string(body))
+  zones := HeartRateZones{}
+  json.Unmarshal(body, &zones)
 
-   z1Seconds := 0
-   z2Seconds := 0
-   z3Seconds := 0
-   z4Seconds := 0
-   z5Seconds := 0
+  fmt.Println("Your Zones")
+  for _, bucket:= range zones.Distribution_buckets {
+    fmt.Println(bucket.Label_long,":", time.Duration(bucket.Time) * time.Second)
+  }
 
-   for j := range activity.Heartrate {
-      if j == 0 { continue }
-      var entry = activity.Heartrate[j]
-      switch {
-         case entry < z2:
-            z1Seconds++
-         case entry >= z2 && entry < z3:
-            z2Seconds++
-         case entry >= z3 && entry < z4:
-            z3Seconds++
-         case entry >= z4 && entry < z5:
-            z4Seconds++
-         case entry >= z4:
-            z5Seconds++         
-      }
-   } 
+ fmt.Println("--------------------------------------------------------")
+  for _, bucket:= range zones.Distribution_buckets {
+    fmt.Println(bucket.Label_short,":", Bar(bucket.Percent*100) )
+    fmt.Println("--------------------------------------------------------")
+  }
 
-   fmt.Println("Total: ", time.Duration(len(activity.Time)-1)*time.Second)
-   fmt.Println("Your Zones")
-   fmt.Println("Zone 1 (Endurance) :", time.Duration(z1Seconds) * time.Second)
-   fmt.Println("Zone 2 (Moderate)  :", time.Duration(z2Seconds) * time.Second)
-   fmt.Println("Zone 3 (Tempo)     :", time.Duration(z3Seconds) * time.Second)
-   fmt.Println("Zone 4 (Threshold) :", time.Duration(z4Seconds) * time.Second)
-   fmt.Println("Zone 5 (Anaerobic) :", time.Duration(z5Seconds) * time.Second)
 
-   fmt.Println("--------------------------------------------------------")
-   fmt.Println("Zone 1", Bar(ZonePercentage(z1Seconds, activity )))
-   fmt.Println("--------------------------------------------------------")
-   fmt.Println("Zone 2", Bar(ZonePercentage(z2Seconds, activity )))
-   fmt.Println("--------------------------------------------------------")
-   fmt.Println("Zone 3", Bar(ZonePercentage(z3Seconds, activity )))
-   fmt.Println("--------------------------------------------------------")
-   fmt.Println("Zone 4", Bar(ZonePercentage(z4Seconds, activity )))
-   fmt.Println("--------------------------------------------------------")
-   fmt.Println("Zone 5", Bar(ZonePercentage(z5Seconds, activity )))
-   fmt.Println("--------------------------------------------------------")
-   
 }
